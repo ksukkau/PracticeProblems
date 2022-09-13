317d2eda4a498c2b068594e582d2f3db69c52df2
Kat Sukkau <Katsukkau@gmail.com>
2022-09-12 20:57:38 -0700

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+
 def running_sum(nums: list[int]) -> list[int]:
-    """
-    Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+    """Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
     Return the running sum of nums.
     Input: nums = [1,2,3,4]
     Output: [1,3,6,10]
