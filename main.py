9d43e0dae3013b92ca49e30e96cc55748d7c458f
Kat Sukkau <Katsukkau@gmail.com>
2022-09-14 20:02:02 -0700

    added isomorphic Strings problem

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def running_sum(nums: list[int]) -> list[int]:
     Input: nums = [1,2,3,4]
     Output: [1,3,6,10]
     Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+    :param nums: list of int
+    :return: list of int
     """
     for i in range(1, len(nums)):
         nums[i] += nums[i - 1]
@@ -15,7 +17,7 @@ def pivot_index(nums: list[int]) -> int:
     The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
     If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
     Return the leftmost pivot index. If no such index exists, return -1.
-    :param nums:
+    :param nums: list of int
     :return: int
     """
     s = sum(nums)
@@ -28,6 +30,32 @@ def pivot_index(nums: list[int]) -> int:
     return -1  # returns -1 if no pivot is found
 
 
+def is_isomorphic(s: str, t: str) -> bool:
+    """
+    Given two strings s and t, determine if they are isomorphic.
+    Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+    All occurrences of a character must be replaced with another character while preserving the order of characters.
+    No two characters may map to the same character, but a character may map to itself.
+    :param s: a String
+    :param t: a String
+    :return: Boolean
+    """
+    mapping_s_t = {}
+    mapping_t_s = {}
+
+    for c1, c2 in zip(s, t):
+
+        if (c1 not in mapping_s_t) and (c2 not in mapping_t_s):
+            mapping_s_t[c1] = c2
+            mapping_t_s[c2] = c1
+
+        elif mapping_s_t.get(c1) != c2 or mapping_t_s.get(c2) != c1:
+            return False
+
+    return True
+
+
 if __name__ == '__main__':
     print(running_sum([1, 2, 3, 4]))
     print(pivot_index([1, 7, 3, 6, 5, 6]))
+    print(is_isomorphic("paper", "title"))
