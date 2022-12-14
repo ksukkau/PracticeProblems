57567e92d8f8779526f9427c5391c565330c2d6f
Kat Sukkau <Katsukkau@gmail.com>
2022-12-13 18:12:01 -0800

    added comments

--- main.py
+++ main.py
@@ -143,7 +143,14 @@ def maxProfit(prices: list[int]) -> int:
 
     return max_profit
 
+
 def isPalindrome(s: str) -> bool:
+    """
+    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+    Given a string s, return true if it is a palindrome, or false otherwise.
+    :param s: String
+    :return: Boolean
+    """
     s = ''.join(ch for ch in s if ch.isalnum())
     s = list(s.lower())
     new_s = []
